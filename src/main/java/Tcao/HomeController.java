1464b519f87dd72a71ce921e43a32c297e65e1f6
Lekang <lekangdu@gmail.com>
2020-02-26 22:09:11 -0500

    Initial commit

---
+++ src/main/java/Tcao/HomeController.java
@@ -0,0 +1,12 @@
+package Tcao;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        System.out.println("This is home page.");
+        return "home";
+    }
+}
