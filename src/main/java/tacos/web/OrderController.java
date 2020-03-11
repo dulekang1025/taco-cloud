7d53c85c46c47c816a7fe9c3a974d91088da035c
Lekang <lekangdu@gmail.com>
2020-03-11 16:09:38 -0400

    Chapter 2

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,27 +1,56 @@
+// tag::baseClass[]
 package tacos.web;
+import javax.validation.Valid;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+//end::baseClass[]
 import org.springframework.web.bind.annotation.PostMapping;
+//tag::baseClass[]
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
+
+import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
+
+    //end::baseClass[]
+//tag::orderForm[]
     @GetMapping("/current")
-    public String orderForm(Model model){
+    public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
     }
+//end::orderForm[]
 
+/*
+//tag::handlePost[]
+  @PostMapping
+  public String processOrder(Order order) {
+    log.info("Order submitted: " + order);
+    return "redirect:/";
+  }
+//end::handlePost[]
+*/
+
+    //tag::handlePostWithValidation[]
     @PostMapping
-    public String processOrder(Order order){
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
+//end::handlePostWithValidation[]
+
+//tag::baseClass[]
+
 }
+//end::baseClass[]
